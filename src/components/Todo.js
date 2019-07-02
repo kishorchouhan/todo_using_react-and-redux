76dc76c9a90286a11576a2d09a3d4af7583c0016
kishorchouhan <devkishorbwr@gmail.com>
2019-07-03 02:23:02 +0530

    completed 3rd task: cross off todo.

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => (
+  <div 
+    style={{
+      textDecoration: props.todo.done ? 'line-through' : ''
+    }} 
+    onClick={props.toggleDone}
+  >
+    {props.todo.text}
+  </div>
+);
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 export default class TodoList extends Component {
   state = {
@@ -12,12 +13,32 @@ export default class TodoList extends Component {
     })
   }
 
+  toggleDone = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          // suppose to update
+          return {
+            ...todo,
+            done: !todo.done
+          };
+        } else {
+          return todo;
+        }
+      })
+    });
+  }
+
   render() {
     return (
       <div>
         <TodoForm onSubmit={this.addTodo} />
         {this.state.todos.map( todo => (
-          <div key={todo.id}>{todo.text}</div>
+          <Todo 
+            key={todo.id} 
+            toggleDone={() => this.toggleDone(todo.id)} 
+            todo={todo}
+          />
         ))}
       </div>
     );
