9beae6961c9da0cf5de13b3a805830d416289fcf
kishorchouhan <devkishorbwr@gmail.com>
2019-07-18 07:44:38 +0530

    Updates

---
+++ precache-manifest.9da85a48995090d39edcf738f030317d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af16096c8b6cf87d3897af1604a696a8",
+    "url": "/todo_using_react-and-redux/index.html"
+  },
+  {
+    "revision": "c34c5ea6501700df63ac",
+    "url": "/todo_using_react-and-redux/static/css/main.748796e8.chunk.css"
+  },
+  {
+    "revision": "0d07436f27fcdda4bc89",
+    "url": "/todo_using_react-and-redux/static/js/2.6e61ca0a.chunk.js"
+  },
+  {
+    "revision": "c34c5ea6501700df63ac",
+    "url": "/todo_using_react-and-redux/static/js/main.fff38c70.chunk.js"
+  },
+  {
+    "revision": "3345b3c739c51c73d2ec",
+    "url": "/todo_using_react-and-redux/static/js/runtime~main.ad86c1ff.js"
+  }
+]);
